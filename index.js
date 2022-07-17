b278953d195f3bb3e9ec81391ec92080f5204879
Vanessa Buecker <v.buecker@rocketmail.com>
2022-07-17 15:39:41 -0300

    Faz pequenas melhorias e cria modal, evento para o botão, funcao para mascarar os 4 ultimos digitos e Algoritmo de Luhn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
-console.log(validator);
+const cardNumberInput = document.querySelector("#card-number")
+const buttonValid = document.querySelector(".button-valid")
+const spanClose = document.querySelector("#close")
+
+buttonValid.addEventListener("click", GetCardNumber);
+
+function GetCardNumber() {
+
+  var element2 = document.getElementById('modal_content');
+  var element = document.getElementById('modal');
+  element.classList.add('show-modal');
+
+  const getTypedValue = cardNumberInput.value;  //get the value typed by the user
+
+  const cardNumberIsValid = validator.isValid(getTypedValue)
+
+  if (cardNumberIsValid) {  //condition displayed, if the card number is valid.
+    element2.innerHTML = ` Olá! O seu cartão foi <b>validado com SUCESSO!</b> <br/> <br/>  <h2><b>${validator.maskify(getTypedValue)}</b></h2> `
+
+  } else { //condition displayed, if the card number is not valid.
+    element2.innerHTML = ` Olá! O seu cartão <b>não pôde</b> ser validado. <br/>
+    ligue para 0800 70 70 para obter mais informações. <br/> <br/> <h2> <b> ${validator.maskify(getTypedValue)} </b></h2>`
+    var img = img.src = "./img/sad.jpg"
+  }
+}
+
+spanClose.addEventListener("click", hideModal);
+
+function hideModal() { // close the modal
+  var element = document.getElementById('modal');
+  element.classList.remove('show-modal');
+}
+
--- validator.js
+++ validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+ //masking the last 4 digits
+ maskify(maskCardNumber) {
+  let hideNumber = [];
+  for (let i = 0; i < maskCardNumber.length; i++) {
+    if (i < maskCardNumber.length - 4) {
+      hideNumber.push('#');
+    } else {
+      hideNumber.push(maskCardNumber[i]);
+    }
+  }
+  return hideNumber.join("");
+},
+
+//Luhn Algorithm
+isValid(cardNumber) {
+  let arr = (cardNumber + '')
+    .split('')
+    .reverse()
+    .map(x => parseInt(x));
+  let lastDigit = arr.splice(0, 1)[0]; //obtain the last digit.
+  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0); //implement the Luhn Algorithm.
+  sum += lastDigit;
+  if (sum % 10 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
 };
+
 
 export default validator;
