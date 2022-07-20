33b960554ac0eee067c7eb0d7734b2360b4681b5
Vanessa Buecker <v.buecker@rocketmail.com>
2022-07-20 17:13:54 -0300

    Faz pequenas modificações e refatora Algoritmo de Luhn

--- validator.js
+++ validator.js
@@ -1,33 +1,45 @@
 const validator = {
- //masking the last 4 digits
- maskify(maskCardNumber) {
-  let hideNumber = [];
-  for (let i = 0; i < maskCardNumber.length; i++) {
-    if (i < maskCardNumber.length - 4) {
-      hideNumber.push('#');
+
+  //masking the last 4 digits
+  maskify(maskCardNumber) {
+    let hideNumber = [];
+    for (let i = 0; i < maskCardNumber.length; i++) {
+      if (i < maskCardNumber.length - 4) {
+        hideNumber.push('#');
+      } else {
+        hideNumber.push(maskCardNumber[i]);
+      }
+    }
+    return hideNumber.join("");
+  },
+
+  //Luhn Algorithm
+  isValid(cardNumber) {
+    let digit, qntDigits, sum, i, len_gth, numberReverse;
+    sum = 0;
+    numberReverse = cardNumber.split('').reverse();
+    for (qntDigits = i = 0, len_gth = numberReverse.length; i < len_gth; qntDigits = ++i) { //++i incrementa o valor de i e retorna o valor já incrementado.
+      digit = numberReverse[qntDigits];
+      digit = +digit;
+      if (qntDigits % 2) {
+        digit *= 2;
+        if (digit < 10) {
+          sum += digit;
+        } else {
+          sum += digit - 9;
+        }
+      } else {
+        sum += digit;
+      }
+    }
+
+    if (sum % 10 === 0) {
+      return true
     } else {
-      hideNumber.push(maskCardNumber[i]);
+      return false
     }
   }
-  return hideNumber.join("");
-},
 
-//Luhn Algorithm
-isValid(cardNumber) {
-  let arr = (cardNumber + '')
-    .split('')
-    .reverse()
-    .map(x => parseInt(x));
-  let lastDigit = arr.splice(0, 1)[0]; //obtain the last digit.
-  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0); //implement the Luhn Algorithm.
-  sum += lastDigit;
-  if (sum % 10 === 0) {
-    return true
-  } else {
-    return false
-  }
-}
 };
-
 
 export default validator;
