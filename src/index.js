1fac0bee3ba609ad17e4c2510b3691fa647851f5
Vanessa Buecker <v.buecker@rocketmail.com>
2022-07-20 17:13:54 -0300

    Faz pequenas modificações e refatora Algoritmo de Luhn

--- src/index.js
+++ src/index.js
@@ -3,29 +3,31 @@ import validator from './validator.js';
 const cardNumberInput = document.querySelector("#card-number")
 const buttonValid = document.querySelector(".button-valid")
 const spanClose = document.querySelector("#close")
+const username = document.querySelector("#user-name")
 
 buttonValid.addEventListener("click", GetCardNumber);
 
 function GetCardNumber() {
 
-  var element2 = document.getElementById('modal_content');
-  var element = document.getElementById('modal');
+  let element2 = document.getElementById('modal_content');
+  let element = document.getElementById('modal');
   element.classList.add('show-modal');
 
-  const getTypedValue = cardNumberInput.value;  //get the value typed by the user
+  const getTypedValue = cardNumberInput.value;
+  const getTypedUsername = username.value;
 
   const cardNumberIsValid = validator.isValid(getTypedValue)
 
-  if (cardNumberIsValid) {  //condition displayed, if the card number is valid.
-    element2.innerHTML = ` Olá! O seu cartão foi <b>validado com SUCESSO!</b> <br/> <br/>  <h2><b>${validator.maskify(getTypedValue)}</b></h2> `
-
-  } else if (getTypedValue == "") { //condition displayed, if the input is empty.
-    element2.style.fontSize = "20px"
-    element2.innerHTML = "Por favor, digite um valor, antes de iniciar a validação!⚠️"
+  if (cardNumberIsValid === true) {  //condition displayed, if the card number is valid.
+    element2.innerHTML = ` Olá, ${getTypedUsername}! O seu cartão foi <b>validado com SUCESSO!</b> <br/> <br/>  <h2><b>${validator.maskify(getTypedValue)}</b></h2> `
 
   } else { //condition displayed, if the card number is not valid.
-    element2.innerHTML = ` Olá! O seu cartão <b>não pôde</b> ser validado. <br/>
+    element2.innerHTML = ` Olá, ${getTypedUsername}! O seu cartão <b>não pôde</b> ser validado. <br/>
     ligue para 0800 70 70 para obter mais informações. <br/> <br/> <h2> <b> ${validator.maskify(getTypedValue)} </b></h2>`
+
+  } if (getTypedValue === "") { //condition displayed, if the input is empty.
+    element2.style.fontSize = "20px"
+    element2.innerHTML = "Por favor, digite um valor, antes de iniciar a validação!⚠️"
   }
 }
 
@@ -35,4 +37,3 @@ function hideModal() { // close the modal
   var element = document.getElementById('modal');
   element.classList.remove('show-modal');
 }
-
